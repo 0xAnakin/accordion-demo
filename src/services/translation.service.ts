eb73ca66395257f23050074b0e27ae1d303f2cb9
0x_Anakin <praditsas@open.gr>
2021-12-02 19:22:53 +0200

    tests removed & cjs module

--- src/services/translation.service.ts
+++ src/services/translation.service.ts
@@ -3,34 +3,19 @@ import { Injectable, Inject, } from '@angular/core';
 import { EL } from '../assets/locales/el.json';
 import { EN } from '../assets/locales/en.json';
 
-const objectResolvePath = require('object-resolve-path');
-
 @Injectable({
   providedIn: 'root'
 })
 export class TranslationService {
 
   private window: any;
-  private locale: string;
+  public i18n: any;
+  public lang_id: string;
 
   constructor(@Inject(DOCUMENT) private document: Document) {
     this.window = this.document.defaultView;
-    this.locale = this.window.Liferay.ThemeDisplay.getLanguageId();
-  }
-
-  translate(key: string) {
-
-
-    const localizationSet = (this.locale === 'el_GR' ? EL : EN);
-
-    // console.debug('set:', localizationSet, 'locale:', this.locale, 'key:', key);
-
-    if (key) {
-      return objectResolvePath(localizationSet, key);
-    }
-
-    return "";
-
+    this.lang_id = this.window.Liferay.ThemeDisplay.getLanguageId();
+    this.i18n = (this.lang_id === 'el_GR' ? EL : EN);
   }
 
 }
