a772f741c90961dfc66c8f62b99e69dad9397ff0
0x_Anakin <praditsas@open.gr>
2021-12-03 15:20:30 +0200

    page not found styling fixes & extra localization

--- src/components/not-found/not-found.component.ts
+++ src/components/not-found/not-found.component.ts
@@ -1,8 +1,11 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { TranslationService } from 'src/services/translation.service';
 
 @Component({
   selector: 'app-not-found-view-component',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
 })
-export class NotFoundComponent {}
+export class NotFoundComponent {
+  constructor(public translationService: TranslationService) { }
+}
